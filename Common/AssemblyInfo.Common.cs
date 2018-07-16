d12c403e423cc161642506a3250bfb23ab351348
文煌 <wwh1004@hotmail.com>
2018-07-16 13:20:15 +0800

    Update AssemblyInfo.Common.cs

--- Common/AssemblyInfo.Common.cs
+++ Common/AssemblyInfo.Common.cs
@@ -1,10 +1,6 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyDescription("Library to load a DLL from memory.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © Wwh 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright © 2018 Wwh")]
 [assembly: AssemblyVersion("1.0.0.2")]
 [assembly: AssemblyFileVersion("1.0.0.2")]
